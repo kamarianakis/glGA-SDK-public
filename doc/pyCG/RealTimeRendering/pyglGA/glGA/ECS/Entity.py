14190e840e8926baa671365adda983191e4bb3af
papagiannakis <papagian@ics.forth.gr>
2020-12-20 20:39:35 +0200

    building up SceneEntity with TDD

--- doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/Entity.py
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/Entity.py
@@ -52,10 +52,33 @@ class Entity(ABC):
 
 
 class SceneEntity(Entity):
-    """the main Entity concrete class of glGA ECS 
     """
+    The main Entity concrete class of glGA ECS 
+    This is the typical equivalent of a Group node in traditional scenegraphs or GameObject in Unity Engine
+    It can contain several other SceneEntity objects as children. 
+    It is an actual data aggregator container of Components. All the actuall operations and logic is performed by 
+    Systems and not the Components or SceneEntity itself.
+    """
+    
+    def __init__(self) -> None:
+        # PEP 256 
+        # note this is how we declare the type of variables in Phython 3.6 and later.
+        # e.g. x: int=1 or x: List[int] = [1] 
+        self._children: List[Entity]=[] 
+       
+    
     def update(self) ->bool:
+        """ Sample function only for subclassing here """
         return True
     
     def transform(self)->bool:
+        """ Sample update() only for subclassing here """
         return True
+
+    def add(self, object: Entity) ->None:
+        self._children.append(object)
+        object._parent = self
+
+    def remove(self, object: Entity) ->None:
+        self._children.remove(object)
+        object._parent = None
--- doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/tests/test_Entity.py
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/tests/test_Entity.py
@@ -34,12 +34,41 @@ class TestSceneEntity(unittest.TestCase):
         """
         SceneEntity init() test
         """
-        print("test_init: START")
+        print("TestSceneEntity:test_init() START")
         gameObject = SceneEntity() 
         
         self.assertIsInstance(gameObject,SceneEntity)
+        self.assertIsInstance(gameObject._children, List)
         
-        print("test_init: END")
+        print(gameObject._children)
+        print("TestSceneEntity:test_init() END")
+
+    def test_add(self):
+        """
+        SceneEntity add() test
+        """
+        print("TestSceneEntity:test_add() START")
+        gameObject = SceneEntity()
+        gameObject2 = SceneEntity()
+        gameObject.add(gameObject2)
+        self.assertIn(gameObject2,gameObject._children)
+        self.assertEqual(gameObject._children[0], gameObject2)
+        #print("gameObject._children[0]" + gameObject._children[0])
+        print("TestSceneEntity:test_add() START")
+    
+    
+    def test_remove(self):
+        """
+        SceneEntity remove() test
+        """
+        print("TestSceneEntity:test_remove() START")
+        gameObject = SceneEntity()
+        gameObject2 = SceneEntity()
+        gameObject.add(gameObject2)
+        gameObject.remove(gameObject2)
+        self.assertNotIn(gameObject2, gameObject._children)
+        #print("gameObject._children[0]" + gameObject._children[0])
+        print("TestSceneEntity:test_remove() START")
 
 if __name__ == "__main__":
     unittest.main(argv=[''], verbosity=3, exit=False)
