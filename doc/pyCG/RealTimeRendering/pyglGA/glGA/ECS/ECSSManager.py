ef59b0f1556f23cddeacbc331421aefac1151ddf
papagiannakis <papagian@ics.forth.gr>
2021-02-25 20:40:11 +0200

    setup ECSSManager and unit tests

--- doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/ECSSManager.py
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/ECSSManager.py
@@ -13,8 +13,52 @@ from typing import List, Dict
 from Entity import Entity
 
 
-class ManagerI(ABC):
-    pass
 
-class ECSSManager(ManagerI):
-    pass
+class ECSSManager():
+    """
+    Singleton Manager class to provide factory creation methods for
+    all Entities, Components, Systems, as an alternative way and hide the scenegraph complexity.
+
+    """
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            print('Creating Scene Singleton Object')
+            cls._instance = super(ECSSManager, cls).__new__(cls)
+            # add further init here
+        return cls._instance
+
+    def __init__(self):
+        self._systems = []
+        self._entities = {}
+        self._components = {}
+        self._next_entity_id = 0
+
+
+    def createEntity(self):
+        pass
+    
+    
+    def addComponent(self):
+        pass
+    
+    
+    def addSystem(self):
+        pass
+    
+    
+    def addEntityChild(self):
+        pass
+
+
+if __name__ == "__main__":
+    # The client code.
+
+    s1 = ECSSManager()
+    s2 = ECSSManager()
+    
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
--- doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/tests/test_ECSSManager.py
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/tests/test_ECSSManager.py
@@ -11,6 +11,26 @@ from Entity import Entity
 from Component import BasicTransform, Camera
 from System import System, TransformSystem, CameraSystem, RenderSystem
 from Scene import Scene
+from ECSSManager import ECSSManager
 
 class TestECSSManager(unittest.TestCase):
-    pass
+    """[summary]
+
+    :param unittest: [description]
+    :type unittest: [type]
+    """
+    
+    def setUp(self):
+        """
+        
+        """
+        self.WorldManager = ECSSManager()
+        self.WorldManager2 = ECSSManager()
+        
+        
+    def test_init(self):
+        """
+        
+        """
+        print(self.WorldManager._next_entity_id)
+        self.assertEqual(id(self.WorldManager), id(self.WorldManager2))
