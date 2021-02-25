62c1d9b806d2f20bc6ceb56843cb8e178e772ced
papagiannakis <papagian@ics.forth.gr>
2021-02-25 13:58:54 +0200

    setting up ECSSManager

---
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/ECSSManager.py
@@ -0,0 +1,20 @@
+"""
+ECSSManager, part of the glGA SDK ECSS
+    
+glGA SDK v2020.1 ECS (Entity Component System)
+@Coopyright 2020 George Papagiannakis
+
+"""
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+from Entity import Entity
+
+
+class ManagerI(ABC):
+    pass
+
+class ECSSManager(ManagerI):
+    pass
---
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/tests/test_ECSSManager.py
@@ -0,0 +1,16 @@
+"""
+Test Scene Unit tests, part of the glGA SDK ECS
+    
+glGA SDK v2020.1 ECS (Entity Component System)
+@Coopyright 2020-2021 George Papagiannakis
+
+"""
+
+import unittest
+from Entity import Entity
+from Component import BasicTransform, Camera
+from System import System, TransformSystem, CameraSystem, RenderSystem
+from Scene import Scene
+
+class TestECSSManager(unittest.TestCase):
+    pass
