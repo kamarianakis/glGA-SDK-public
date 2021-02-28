36e7a290ecb9f52e786f36e8687cb33eba766302
papagiannakis <papagian@ics.forth.gr>
2021-02-28 21:49:51 +0200

    create methods of ECSSManager

--- doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/ECSSManager.py
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/ECSSManager.py
@@ -11,7 +11,9 @@ from abc import ABC, abstractmethod
 from typing import List, Dict
 
 from Entity import Entity
-
+import Component
+import System
+import utilities as util
 
 
 class ECSSManager():
@@ -23,6 +25,12 @@ class ECSSManager():
     _instance = None
     
     def __new__(cls):
+        """
+        Special singleton class method, returns single instance of ECSSManager
+
+        :return: single class instance
+        :rtype: ECSSManagger
+        """
         if cls._instance is None:
             print('Creating Scene Singleton Object')
             cls._instance = super(ECSSManager, cls).__new__(cls)
@@ -30,26 +38,96 @@ class ECSSManager():
         return cls._instance
 
     def __init__(self):
-        self._systems = [] #list for all systems
-        self._entities = {} #dict with keys entities and values list of components per entity
-        self._components = {} #dict with keys component types and values component
+        """
+        Construct initial data structures for scenegraph elements
+        """
+        self._systems: List[System.System]=[] #list for all systems
+        self._components: List[Component.Component]=[] #list with all scenegraph components
+        self._entities: List[Entity]=[] #list of all scenegraph entities
+        self._cameras: List[Component.Component]=[] # list of all scenegraph camera components
+        self._entities_components = {} #dict with keys entities and values list of components per entity
         self._next_entity_id = 0
+        self._root = None
 
-
-    def createEntity(self, entity: Entity)->bool:
-        self._entities[entity] = [None] #add an empty list for components with the new Entity
         
-        return True
+
+    def createEntity(self, entity: Entity):
+        """
+        Creates an Entity in the underlying scenegraph and adds it in the ECSS data structures.
+        
+        Checks if the Entity's name is "root" to add it as root of the ECSS
+        
+        :param entity: Entity to add in the Scenegraph
+        :type entity: Entity
+        """
+        if isinstance(entity, Entity):
+            self._entities.append(entity) #add an empty list for components with the new Entity
+            self._entities_components[entity]=[None]
+        
+            if entity.name.lower() == "root":
+                self._root = entity
+        
+        return entity #if the method was called with an inline constructor e.g. 'createEntity(Entity())', 
+                        # we return that created Entity in case it is needed
     
-    def addComponent(self):
-        pass
+    
+    def createSystem(self, system: System.System):
+        """
+        Creates a System and adds it in the ECSS data structures
+        
+        """
+        if isinstance(system, System.System):
+            self._systems.append(system)
+        
+        return system
     
     
-    def addSystem(self):
+    def createIterator(self, entity: Entity, dfs=True):
+        """
+        Creates and returns a scenegraph traversal node iterator
+
+        :param entity: [description]
+        :type entity: Entity
+        """
+        if isinstance(entity, Entity):
+            if dfs:
+                return iter(entity)
+        
+        
+    
+    def addComponent(self, entity: Entity, component: Component.Component):
+        """
+        Adds a component to an Entity in a scenegraph and in the ECSS data structures
+        
+        Checks if that Component is a Camera, to add it in the list of Cameras
+        
+        Checks if that Entity has already such a component of that type and replaces 
+        it with the new one
+        
+        Checks that indeed only a component is added with this method. 
+        If we need to add a child Entity to an Enity, we use addEntityChild()
+
+        :param entity: Parent Entity
+        :type entity: Entity
+        :param component: The component to be added to this Entity
+        :type component: Component
+        """
         pass
     
     
     def addEntityChild(self, entity_parent: Entity, entity_child: Entity):
+        """
+        Adds a child Enity to a parent one and thus establishes a hierarchy 
+        in the underlying scenegraph.
+        
+        Adds the child Entity also in the ECSS _entities_components dictionary 
+        data structure, so that the hierarchy is also visible at ECSSManager level.
+
+        :param entity_parent: [description]
+        :type entity_parent: Entity
+        :param entity_child: [description]
+        :type entity_child: Entity
+        """
         pass
 
 
--- doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/tests/test_ECSSManager.py
+++ doc/pyCG/RealTimeRendering/pyglGA/glGA/ECS/tests/test_ECSSManager.py
@@ -7,7 +7,7 @@ glGA SDK v2020.1 ECS (Entity Component System)
 """
 
 import unittest
-from Entity import Entity
+from Entity import Entity, EntityDfsIterator
 from Component import BasicTransform, Camera
 from System import System, TransformSystem, CameraSystem, RenderSystem
 from Scene import Scene
@@ -22,6 +22,14 @@ class TestECSSManager(unittest.TestCase):
     
     def setUp(self):
         """
+        Sets up the ECSS both at ECSSManager level data structures
+        as well as the underlying scenegraph
+        
+        mimicks the scenegraph created in TestCameraSystem setUp().
+        
+        hence we have two ways of setting up an ECSS: 
+        a) High-level via the ECSSManager (recommended method)
+        b) Low-level directly at scenegraph hierarchy level
         
         """
         self.WorldManager = ECSSManager()
@@ -35,10 +43,25 @@ class TestECSSManager(unittest.TestCase):
         
         """
         
-        self.WorldManager.createEntity(Entity(name="root"))
+        print("TestECSSManager:test_init START".center(100, '-'))
         
-        for key in self.WorldManager._entities.keys():
-            print("\n entity: ",key, ":: with components: ", self.WorldManager._entities[key])
+        rootEntity = self.WorldManager.createEntity(Entity(name="RooT"))
+        
+        transUpdate = self.WorldManager.createSystem(TransformSystem("transUpdate", "TransformSystem", "001"))
+        camUpdate = self.WorldManager.createSystem(CameraSystem("camUpdate", "CameraUpdate", "200"))
+        dfsIterator = self.WorldManager.createIterator(rootEntity)
+        
+        
+        
+        for key, value in self.WorldManager._entities_components.items():
+            print("\n entity: ",key, ":: with components: ", value)
         
         print(self.WorldManager._next_entity_id)
         self.assertEqual(id(self.WorldManager), id(self.WorldManager2))
+        self.assertEqual(rootEntity, self.WorldManager._root)
+        self.assertIsInstance(transUpdate, TransformSystem)
+        self.assertIsInstance(camUpdate, CameraSystem)
+        self.assertIsInstance(dfsIterator, EntityDfsIterator)
+        
+        
+        print("TestECSSManager:test_init END".center(100, '-'))
