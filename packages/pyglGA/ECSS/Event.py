95d14146c207c38c79a9e703a382ba75460e515f
papagiannakis <papagian@ics.forth.gr>
2021-04-20 19:43:36 +0300

    building up Event and EvenManager classes and tests

--- packages/pyglGA/ECSS/Event.py
+++ packages/pyglGA/ECSS/Event.py
@@ -8,3 +8,58 @@ based on the Mediator and Observer design patterns.
 
 """
 
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List, Dict, Any
+from dataclasses import dataclass
+
+import numpy as np
+
+import pyglGA.ECSS.utilities as util
+from pyglGA.ECSS.Entity import Entity, EntityDfsIterator
+from pyglGA.ECSS.Component import BasicTransform, Camera, Component
+from pyglGA.ECSS.System import System, TransformSystem, CameraSystem, RenderSystem
+from pyglGA.ECSS.ECSSManager import ECSSManager
+from pyglGA.GUI.Viewer import SDL2Window, ImGUIDecorator
+
+@dataclass
+class Event:
+    """A simple dataclass that encapsulates an Event
+    """
+    name: str
+    id: Any
+    value: Any
+
+
+class EventPublisher(ABC):
+    """
+    Interface class for all EventManagers that act as Publishers/Subjects (based on the Observer design pattern)
+    or as Mediators (based on the Mediator design pattern) 
+    """
+    
+    @abstractmethod
+    def notify(self, component: Component, event: Event):
+        raise NotImplementedError
+    
+
+class EventManager(EventPublisher):
+    """
+    Main Mediator (Subject/Publisher) class that contains list of Observers/Subscribers (Components) that is being 
+    subscribed (notified) from and delegates to Systems to act upon these events invoked from these Components.
+    """
+    
+    def __init__(self):
+        self._subscribers: List[Component]=[]
+    
+    def notify(self, component: Component, event: Event):
+        print(f'\n{EventManager.getClassName()}: notify() reacts from {component} with {event}\n')
+    
+    def subscribe(self, component: Component):
+        self._subscribers.append(component)
+        
+    def unsubscribe(self, component: Component):
+        self._subscribers.remove(component)
+    
+    @classmethod
+    def getClassName(cls):
+        return cls.__name__    
--- packages/pyglGA/ECSS/tests/test_Event.py
+++ packages/pyglGA/ECSS/tests/test_Event.py
@@ -15,4 +15,30 @@ import pyglGA.ECSS.utilities as util
 from pyglGA.ECSS.System import System, TransformSystem, CameraSystem
 from pyglGA.ECSS.Entity import Entity
 from pyglGA.ECSS.Component import BasicTransform, Camera
+from pyglGA.ECSS.Event import Event
 
+class TestEvent(unittest.TestCase):
+    
+    def test_init(self):
+        """simple tests for Event dataclass
+        """
+        trsMat = util.translate(10.0,20.0,30.0)
+        e = Event("OnUpdate", 100, trsMat)
+        
+        mT = np.array([
+            [1.0,0.0,0.0,10.0],
+            [0.0,1.0,0.0,20.0],
+            [0.0,0.0,1.0,30.0],
+            [0.0,0.0,0.0,1.0],
+        ],dtype=np.float,order='F')
+        
+        self.assertEqual(mT.tolist(), e.value.tolist())
+        self.assertEqual(e.name, "OnUpdate")
+        self.assertEqual(e.id, 100)
+        np.testing.assert_array_equal(e.value,mT)
+        
+        e.id = 101
+        self.assertEqual(e.id, 101)
+        
+        print(e.value)
+        print("\n Event e: ",e)
